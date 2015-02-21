ae8a1f93cf69497a5e824fb31334d01cf5d36557
Antropovi <antropovi@yahoo.com>
2015-02-22 02:19:40 +0300

    DST and Peano

---
+++ BinarySearchTree/BST.fs
@@ -0,0 +1,73 @@
+﻿// Antropov Igor 21.02.2015
+// Binary Search Tree
+
+
+type tree = Nil | Node of tree * int * tree
+
+let rec insert tree value =
+  match tree with
+  | Nil -> Node (Nil, value, Nil)
+  | Node (l, c, r) -> 
+    if c = value then Node (l, value, r) 
+      elif c > value then Node (insert l value, c, r) 
+        else Node (l, c, insert r value)
+  
+let rec printСLR l =
+  match l with
+  | Nil -> printf ""
+  | Node (l, c, r) -> printf "%i " c; printСLR l; printСLR r
+
+let rec printLRC l =
+  match l with
+  | Nil -> printf ""
+  | Node (l, c, r) -> printLRC l; printLRC r; printf "%i " c
+
+let rec printLCR l =
+  match l with
+  | Nil -> printf ""
+  | Node (l, c, r) -> printLCR l; printf "%i " c; printLCR r
+
+let rec right tree =
+  match tree with
+  | Nil -> Nil
+  | Node (l, c, r) -> r
+
+let rec left tree =
+  match tree with
+  | Nil -> Nil
+  | Node (l, c, r) -> l
+
+let rec center tree =
+  match tree with
+  | Nil -> 0
+  | Node (l, c, r) -> c
+
+let rec find_most_left tree =
+  match tree with
+  | Nil -> 0
+  | Node (l, c, r) -> if l = Nil then c else find_most_left l
+
+let rec move_left_subtree tree =
+  match tree with
+  | Nil -> Nil
+  | Node (l, c, r) -> 
+    if left l = left Nil then Node (right l, c, r)
+                    else Node (move_left_subtree l, c, r)
+
+let rec remove tree value =
+  match tree with
+  | Nil -> Nil
+  | Node (l, c, r) -> 
+    if c < value then Node (remove l value, c, r)
+      elif c > value then Node (l, c, remove r value)
+      elif c = value && l = Nil && r = Nil then Nil
+        elif c = value && l <> Nil && r = Nil then l
+          elif c = value && l = Nil && r <> Nil then r
+            elif left r = left Nil 
+              then Node (l, center r, right r)
+                else Node (l, find_most_left r, move_left_subtree r)
+
+
+[<EntryPoint>]
+let main args =
+    0 
---
+++ PeanoNumbers/Peano.fs
@@ -0,0 +1,49 @@
+﻿//Antropov Igor 21.02.2015
+//Peano numbers
+
+type Peano = Zero | S of Peano
+
+let Suc p = S p
+
+let minus1 p = 
+ match p with
+ | Zero -> Zero
+ | S p -> p
+
+let rec plus a b =
+ match a with
+ | Zero -> b
+ | S a -> S (plus a b)
+
+let rec minus a b =
+  match a,b with
+  | Zero, _ -> Zero
+  | S a, Zero -> S a
+  | S a, S b -> minus a b
+
+let rec mult a b =
+  match a with
+  | Zero -> Zero
+  | S a -> plus b (mult a b)
+
+let rec pow a b = 
+  match a, b with 
+  | _ , Zero -> S(Zero)
+  | Zero, S b -> Zero
+  | S a, S b -> mult (S a) (pow (S a) b)
+
+let rec toInt p = 
+  match p with 
+  | Zero -> 0
+  | S p -> toInt (p) + 1 
+
+let intToString p =
+  p.ToString ()
+
+let print p = p |> toInt
+                |> intToString
+                |> printf "%s/n"
+
+[<EntryPoint>]
+let main argv = 
+  0
