23e2bebe4879d40e4348b5292845df982537212d
Antropovi <antropovi@yahoo.com>
2015-03-29 07:19:28 +0300

    Added 3rd homework

---
+++ hw03/part1.fs
@@ -0,0 +1,90 @@
+﻿
+//#20
+type IGraph<'A> =
+  interface 
+    abstract IsPassable : 'A -> 'A -> bool
+    abstract GetSize : unit -> int
+    abstract GetVals : unit -> 'A array
+  end
+
+//#21
+type GraphMatrix<'A when 'A : equality> (values : 'A array, edges : bool [,]) = 
+  class
+    let size = values.Length
+    let vert = values 
+    interface IGraph<'A> with
+      member this.GetSize () = size
+      member this.GetVals () = vert
+      member this.IsPassable a b = 
+        let temp1 = Array.findIndex (fun x -> x = a) values
+        let temp2 = Array.findIndex (fun x -> x = b) values
+        edges.[temp1,temp2]
+  end
+
+//#22
+type ListGraph<'A when 'A : equality>(values : 'A array, edges : list<'A> array) = 
+  class
+    let size = values.Length
+    let vert = values
+    interface IGraph<'A> with
+      member this.GetVals () = vert
+      member this.GetSize () = size
+      member this.IsPassable a b = 
+        let temp1 = Array.findIndex (fun x -> x = a) values
+        List.exists (fun x -> x = b) edges.[temp1]
+  end
+
+
+let rec dfs (graph : IGraph<'A>) (visited : bool array) value dir = 
+  if dir = "In" then 
+    let index = Array.findIndex (fun x -> x = value) (graph.GetVals ())
+    for i in 0 .. (graph.GetSize ()) - 1 do
+      if not visited.[i] && graph.IsPassable value (graph.GetVals ()).[i]  
+        then visited.[i] <- true 
+             dfs graph visited (graph.GetVals ()).[i] dir
+  else 
+    let index = Array.findIndex (fun x -> x = value) (graph.GetVals ())
+    for i in 0 .. (graph.GetSize ()) - 1 do
+      if not visited.[i] && graph.IsPassable (graph.GetVals ()).[i] value 
+        then visited.[i] <- true 
+             dfs graph visited (graph.GetVals ()).[i] dir                       
+  visited |> ignore
+
+
+let visitedToVals (visited : bool array) (values : 'A array) =
+  let mutable result = []
+  for i in 0 .. visited.Length - 1 do 
+    if visited.[i] then result <- List.append result [values.[i]]
+  result
+
+//#23
+let passableTo (graph : IGraph<'A>) value = 
+  let mutable result = []
+  let size = graph.GetSize()
+  let visited = Array.create size false
+  dfs graph visited value "In"
+  visitedToVals visited (graph.GetVals())
+
+
+//#24
+let passableFrom (graph : IGraph<'A>) value = 
+  let mutable result = []
+  let size = graph.GetSize()
+  let visited = Array.create size false
+  dfs graph visited value "Out"
+  visitedToVals visited (graph.GetVals())
+
+
+
+//#25
+type ILabelGraph<'A, 'B> =
+  interface 
+    inherit  IGraph<'A>
+    abstract GetLabels : unit -> 'B array
+  end
+
+
+
+[<EntryPoint>]
+let main argv = 
+    0 // return an integer exit code
---
+++ hw03/part2.fs
@@ -0,0 +1,129 @@
+﻿
+
+type IList<'A when 'A : equality> =
+  interface 
+    abstract AddHead   : 'A -> unit
+    abstract AddTail   : 'A -> unit
+    abstract AddByNum  : 'A -> int -> unit
+    abstract DellHead  : unit -> unit
+    abstract DellTail  : unit -> unit
+    abstract DellByNum : int -> unit
+    abstract FindByVal : ('A -> bool) -> Option<'A>
+    abstract Head      : Option<'A> 
+    abstract Append    : IList<'A> -> unit
+  end
+
+
+  type List<'A> = Null | Node of 'A * List<'A> 
+
+
+  type ADTList<'A when 'A : equality> (L: List<'A>) =
+    class
+      let mutable list = L 
+
+      interface IList<'A> with 
+        member this.AddHead elem = list <- Node (elem, list)
+
+        member this.AddTail elem = 
+          let rec pushTo list =
+            match list with
+            | Null -> Node (elem, Null)
+            | Node (a, b) -> Node (a,pushTo b)
+          list <- pushTo list
+
+        member this.AddByNum elem num = 
+          let rec func list count = 
+            match list with 
+            | Null -> Node (elem, Null)
+            | Node (a, b) -> 
+              if count = num then Node (elem, list) else Node (a, func b (count+1) )
+          list <- func list 0
+
+        member this.DellHead () = 
+          match list with
+          | Null -> ()
+          | Node (a,b) -> list <- b 
+
+        member this.DellTail () = 
+          let rec func list =
+            match list with
+            | Null -> Null
+            | Node (a, Node( _ , Null)) -> Node (a, Null)
+            | Node (a, b) -> Node (a, func b)
+          list <- func list
+
+        member this.DellByNum num = 
+          let rec func list count =
+            match list with
+            | Null -> Null
+            | Node (a, b) -> if count = num then b else Node (a, func b (count+1)) 
+          list <- func list 0
+
+        member this.FindByVal f = 
+          let rec func list = 
+            match list with
+            | Null -> failwith "Нет такого элемента"
+            | Node (a, b) -> if (f a) then Some a else func b
+          func list
+
+        member this.Head = 
+          match list with
+          | Null -> None
+          | Node (a, _) -> Some a
+
+
+        member this.Append (l : IList<'A>) = 
+          let rec func list = 
+            match list with 
+            | Node (a, b) -> Node(a, func b)
+            | Null ->
+              match l.Head with               
+              | Some x -> l.DellHead(); Node (x, func list)
+              | None -> Null
+          list <- func list
+    end
+
+
+
+
+
+
+  type ArrayList<'A when 'A : equality> (A : 'A[]) =
+    class
+      let mutable array = A
+
+      interface IList<'A> with 
+        member this.AddHead elem = array <- Array.append [|elem|] array
+
+        member this.AddTail elem = array <- Array.append array [|elem|]
+
+        member this.AddByNum elem num =          
+         array <- Array.append 
+                   (Array.append (Array.sub array 0 num) [|elem|])
+                   (Array.sub array num (array.Length - num + 1) )
+
+        member this.DellHead () = array <- Array.sub array 1 (array.Length - 1)
+        member this.DellTail () = array <- Array.sub array 0 (array.Length - 1)
+
+        member this.DellByNum num = 
+          array <- Array.append 
+                   (Array.sub array 0 (num - 1)) 
+                   (Array.sub array (num + 1) (array.Length - num))
+
+        member this.FindByVal func = Array.tryFind func array
+
+        member this.Head = Some array.[0]
+
+        member this.Append l = 
+          let rec func () =
+            match l.Head with               
+            | Some x -> array <- Array.append array [|x|]; l.DellHead(); func() 
+            | None -> ()
+          func ()
+    end
+
+
+
+[<EntryPoint>]
+let main argv =
+    0
